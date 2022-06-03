682543d6fc97cb987132b27e24c0c228ebdfe976
Nils <olivernils8@gmail.com>
2022-06-03 22:36:40 +0200

    Removed some files onto both the camera module and the renderer module.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from entities.entities import*
 from entities.camera import*
 import pyrr
 import random
-from entities.light import*
 
 class Game:
 	def __init__(self, width, height):
@@ -20,32 +19,16 @@ class Game:
 		self.display = pygame.display.set_mode((self.width, self.height), pygame.OPENGL | pygame.DOUBLEBUF)
 		self.clock = pygame.time.Clock()
 		self.delta_tick = pygame.time.get_ticks()
+		# Make the cursor insivible. This should be changed later so we can controll if we want to see the cursor or not.
 		pygame.mouse.set_cursor((8,8),(1,1),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
 
 		self.shader = StaticShader()
+		self.shader.create_perspective_projection(self.width, self.height)
 		self.camera = Camera(self.width, self.height)
-		self.light = Light([0.0, 2.0, 0.0], [1.0, 1.0, 1.0])
-
-		# Setting up the lights.
-		self.light_pos_loc = glGetUniformLocation(self.shader.get_id(), "light_position")
-		self.light_color_loc = glGetUniformLocation(self.shader.get_id(), "light_color")
-		self.light_pos = self.light.get_pos()
-		self.light_color = self.light.get_color()
-		glUniform3f(self.light_pos_loc, self.light_pos[0], self.light_pos[1], self.light_pos[2])
-		glUniform3f(self.light_color_loc, self.light_color[0], self.light_color[1], self.light_color[2])
-		# TODO: move this and many other functions related to shader uniforms to the shader instead of the game loop.
-
-		# Setting the projection for the scene.
-		self.projection = pyrr.matrix44.create_perspective_projection_matrix(45, 1280 / 720, 0.1, 100)
-		self.proj_loc = glGetUniformLocation(self.shader.get_id(), "projection")
-		glUniformMatrix4fv(self.proj_loc, 1, GL_FALSE, self.projection)
+		self.light = self.shader.add_light([0.0, 2.0, 0.0], [1.0, 1.0, 1.0])
 
 		# Reference the state that should be run in the main loop.
 		self.state = GameState(self)
-
-	def change_view_matrix(self, matrix): # Update the view matrix gotten from the camera entity.
-		self.view_loc = glGetUniformLocation(self.shader.get_id(), "view")
-		glUniformMatrix4fv(self.view_loc, 1, GL_FALSE, matrix)
 
 	def camera_movement(self, keys):
 		if keys[pygame.K_w]:
@@ -70,10 +53,10 @@ class Game:
 					self.running = False
 
 			keys = pygame.key.get_pressed()
+			self.camera_movement(keys)
 			if keys[pygame.K_ESCAPE]:
 				self.running = False
 
-			self.camera_movement(keys)
 
 			# DeltaTime setup.
 			self.delta_time = (pygame.time.get_ticks() - self.delta_tick) / 50
@@ -84,7 +67,7 @@ class Game:
 			# View and camera logic.
 			mouse_pos = pygame.mouse.get_pos()
 			self.camera.process_mouse_movement(mouse_pos[0], mouse_pos[1])
-			self.change_view_matrix(self.camera.get_view_matrix())
+			self.shader.change_view_matrix(self.camera.get_view_matrix())
 
 			self.clock.tick(60)
 			pygame.display.flip()
